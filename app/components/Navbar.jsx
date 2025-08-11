12fd7bec2a2b5529e4a7193a8e3e4476a277f65f
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-08-11 12:39:22 +0800

    -fixed tailwind problem
    -finished navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import { assets } from '@/assests/assets';
+
+
+
+const Navbar = () => {
+  const sideMenuRef = useRef();
+  const openMenu = ()=> {
+    sideMenuRef.current.style.transform = 'translateX(-16rem)'
+  }
+
+  const closeMenu = ()=> {
+    sideMenuRef.current.style.transform = 'translateX(16rem)'
+  }
+
+  return (
+    <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='' className='w-full' />
+    </div>
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
+      flex items-center justify-between z-50'>
+        <a href="#">
+          <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt='logo'/>
+        </a>
+
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 text-xl'>
+          <li><a className='font-Outfit' href="#top">Home</a></li>
+          <li><a className='font-Outfit' href="#about">About me</a></li>
+          <li><a className='font-Outfit' href="#education">Education</a></li>
+          <li><a className='font-Outfit' href="#work">My Work</a></li>
+        </ul>
+
+        <div className='flex items-center gap-4'>
+          <button><Image src={assets.moon_icon} alt='darkModeIcon' className='w-10 l-10 ml-4 lg-flex gap-3 items-center'/></button>
+          
+          <button className='block md:hidden ml-3' onClick={openMenu} >
+          <Image src={assets.menu_black} alt='' className='w-6 cursor-pointer'/></button>
+        
+        </div>
+
+        {/*-- Mobile Menu --*/}
+
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-purple-50 transition duration-500'>
+          <div className='absolute right-6 top-6' onClick={closeMenu} >
+            <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+          </div>
+
+          <li><a className='font-Outfit' onClick={closeMenu} href="#top">Home</a></li>
+          <li><a className='font-Outfit' onClick={closeMenu} href="#about">About me</a></li>
+          <li><a className='font-Outfit' onClick={closeMenu} href="#education">Education</a></li>
+          <li><a className='font-Outfit' onClick={closeMenu} href="#work">My Work</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
+
