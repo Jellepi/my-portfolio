b4c14d02ca3c1073b7b7f1d6f8638d9ae882fb5b
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-08-16 21:08:08 +0800

    opens certificate on new tab

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -22,7 +22,7 @@ const Experience = () => {
                 <Image src={icon} alt="icon" className="w-10" />
                 <h3 className="text-lg my-4 text-gray-700">{title}</h3>
                 <p className="text-sm text-gray-600 leading-5">{description}</p>
-                <a href={link} className="cursor-pointer flex items-center gap-2 text-sm mt-5">
+                <a href={link} target="_blank" className="cursor-pointer flex items-center gap-2 text-sm mt-5">
                 View Certificate
                 <Image alt="" src={assets.right_arrow} className="w-4" />
                 </a>
@@ -31,7 +31,6 @@ const Experience = () => {
 
         </div>
 
-    
 
     </div>
   )
