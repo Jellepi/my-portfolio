480a16366ffa9fabc577fd34e12695f57e3fe851
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-08-17 20:14:34 +0800

    Finished Footer

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
   };
 
   return (
-    <div id='contact' className='h-screen w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
       <h4 className='text-center mb-2 text-xl font-ovo'>Connect with me</h4>
       <h2 className='text-center text-5xl'>Get in touch</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assests/assets'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='mt-20'>
+      <div className='text-center'>
+        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+        <div className='w-max flex items- center gap-2 mx-auto'>
+        <Image src={assets.mail_icon} alt='' className='w-6 mt-2 h-5'/>
+        jella.pintor1@gmail.com
+        </div>       
+      </div>
+    <div className='text-center sm:flex items-center justify-between
+    border-t border-gray-400 mx-[10%] mt-12 py-6'>
+        <p> © 2025 Jella Pintor. All rights reserved</p>
+        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+            <li><a target='_blank'href="https://github.com/Jellepi">Github</a></li>
+            <li><a target='_blank'href="https://www.linkedin.com/in/jellapintor/">Linkedin</a></li>
+        </ul>
+    </div>
+
     </div>
   )
 }
