a0ad1ebb2e96755f8eeaca4d5a721e2f42df276c
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-08-18 19:47:01 +0800

    Impelemented darkmode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { assets } from '@/assests/assets';
 
-const Navbar = () => {
+const Navbar = ({isDarkMode, setIsDarkMode}) => {
 
   const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
@@ -26,14 +26,14 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
       <Image src={assets.header_bg_color} alt='' className='w-full' />
     </div>
 
       <nav className= {`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
-      flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : "" } `}>
+      flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkHover dark:shadow-white/20" : "" } `}>
         <a href="#">
-          <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt='logo'/>
+          <Image src={isDarkMode ? assets.logo_dark: assets.logo} className='w-28 cursor-pointer mr-14' alt='logo'/>
         </a>
 
         <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 text-xl'>
@@ -45,18 +45,20 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-          <button><Image src={assets.moon_icon} alt='darkModeIcon' className='w-10 l-10 ml-4 lg-flex gap-3 items-center'/></button>
+          <button onClick={()=>setIsDarkMode(prev => !prev)}>
+            <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='darkModeIcon' className='w-10 l-10 ml-4 lg-flex gap-3 items-center cursor-pointer'/></button>
           
           <button className='block md:hidden ml-3' onClick={openMenu} >
-          <Image src={assets.menu_black} alt='' className='w-6 cursor-pointer'/></button>
+          <Image src={ isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6 cursor-pointer'/></button>
         
         </div>
 
         {/*-- Mobile Menu --*/}
 
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-irides4 transition duration-500 text-xl'>
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 
+        z-50 h-screen bg-irides4 transition duration-500 text-xl dark:bg-darkHover dark:text-white'>
           <div className='absolute right-6 top-6' onClick={closeMenu} >
-            <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+            <Image src={isDarkMode? assets.close_white:assets.close_black} alt='' className='w-5 cursor-pointer'/>
           </div>
 
           <li><a className='font-outfit' onClick={closeMenu} href="#top">Home</a></li>
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -8,15 +9,37 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  useEffect(()=>{
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) &&
+     window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true)
+    }else{
+      setIsDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  }, [isDarkMode])
+
+
   return (
    <>
-   <Navbar      />
-   <Hero        />
-   <About       />
-   <Experience  />
-   <Projects    />
-   <Contact     />
-   <Footer      />
+   <Navbar    isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}   />
+   <Hero      isDarkMode={isDarkMode}   />
+   <About      isDarkMode={isDarkMode}   />
+   <Experience isDarkMode={isDarkMode} />
+   <Projects  isDarkMode={isDarkMode}  />
+   <Contact   isDarkMode={isDarkMode}  />
+   <Footer    isDarkMode={isDarkMode}  />
    </>
   );
 }
