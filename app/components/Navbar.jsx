5ba298f606d77e9bca67c49e6113632fd18522dc
Jellepi <129751198+Jellepi@users.noreply.github.com>
2025-08-19 12:57:01 +0800

    small color changes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
     </div>
 
       <nav className= {`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
-      flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkHover dark:shadow-white/20" : "" } `}>
+      flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkVioletBlack dark:shadow-white/10" : "" } `}>
         <a href="#">
           <Image src={isDarkMode ? assets.logo_dark: assets.logo} className='w-28 cursor-pointer mr-14' alt='logo'/>
         </a>
@@ -56,7 +56,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
         {/*-- Mobile Menu --*/}
 
         <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 
-        z-50 h-screen bg-irides4 transition duration-500 text-xl dark:bg-darkHover dark:text-white'>
+        z-50 h-screen bg-irides4 transition duration-500 text-xl dark:bg-darkVioletBlack dark:text-white'>
           <div className='absolute right-6 top-6' onClick={closeMenu} >
             <Image src={isDarkMode? assets.close_white:assets.close_black} alt='' className='w-5 cursor-pointer'/>
           </div>
